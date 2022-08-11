f482c92e5f021ca776c18a8bc3af544da69ecc5e
nggar <enggarjpx@gmail.com>
2022-08-11 21:18:02 +0700

    show packages from the api

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,34 @@
+import React, { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
+
 const RepositoriesList: React.FC = () => {
+    const [term, setTerm] = useState('');
+    const { searchRepositories } = useActions();
+    const { data, error, loading } = useSelector(
+        (state: any) => state.repositories
+    );
+
+    const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        searchRepositories(term);
+    };
     return (
         <div>
-            <form>
-                <input type="text" />
-                <button>Search</button>
+            <form onSubmit={formSubmitHandler}>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                />
+                <button type="submit">Search</button>
             </form>
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading...</h3>}
+            {!error &&
+                !loading &&
+                data.map((name: any) => <div key={name}>{name}</div>)}
         </div>
     );
 };
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,2 +1,2 @@
 export * from './store';
-export * as ActionCreators from './action-creators';
+export * as actionCreators from './action-creators';
