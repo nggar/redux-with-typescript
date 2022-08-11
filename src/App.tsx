330d89fca414ffe0d1d59ff644be1fc3e6bcfc1f
nggar <enggarjpx@gmail.com>
2022-08-11 09:18:40 +0700

    wrapping up to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import RepositoriesList from './components/RepositoriesList';
+
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <div className="App">
+            <h1>Search for a Package</h1>
+            <RepositoriesList />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,11 @@
+const RepositoriesList: React.FC = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <button>Search</button>
+            </form>
+        </div>
+    );
+};
+export default RepositoriesList;
